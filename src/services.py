84b72cc8501f729726bfbf1152406d1661bdb6f5
dabide <dabide0812@gmail.com>
2024-11-19 17:29:15 +0900

    基本的な演算処理

--- src/services.py
+++ src/services.py
@@ -2,7 +2,20 @@ class StringService:
     def get_output(self) -> str:
 
         #演算処理関連の動作確認実施
+        output = ''
+        output = output + '足し算<br>'
+        output = output + '7 + 2 = ' + str( 7 + 2 ) + '<br><br>'
+        output = output + '引き算<br>'
+        output = output + '7 - 2 = ' + str( 7 - 2 ) + '<br><br>'
+        output = output + '掛け算<br>'
+        output = output + '7 * 2 = ' + str( 7 * 2 ) + '<br><br>'
+        output = output + 'べき乗<br>'
+        output = output + '7 ** 2 = ' + str( 7 ** 2 ) + '<br><br>'
+        output = output + '割り算<br>'
+        output = output + '7 / 2 = ' + str( 7 / 2 ) + '<br><br>'
+        output = output + '整数除算<br>'
+        output = output + '7 // 2 = ' + str( 7 // 2 ) + '<br><br>'
+        output = output + '余剰<br>'
+        output = output + '7 % 2 = ' + str( 7 % 2 ) + '<br><br>'
 
-        i = 1 + 1
-
-        return i
+        return output
