1afa57a5354ef23b15d660dfef9adb45c9b11a2b
dabide <dabide0812@gmail.com>
2024-11-19 16:42:45 +0900

    文字列出力の確認ができるように修正

---
+++ src/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./src/templates/**/*.html",  // HTMLテンプレートのパスを指定
+      "./src/**/*.py",              // 必要に応じてPythonファイルも監視
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
