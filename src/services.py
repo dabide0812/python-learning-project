250852b91fc1959366d7848aae381788ea2548e9
dabide <dabide0812@gmail.com>
2024-11-20 10:27:24 +0900

    mathクラスの利用を確認

--- src/services.py
+++ src/services.py
@@ -2,41 +2,22 @@ class StringService:
     def get_output(self) -> str:
 
         #演算処理関連の動作確認実施
-        output = ''
-        output = output + '実数での除算<br>'
-        output = output + '7 / 1 = ' + str( 7 / 1 ) + '<br>'
-        output = output + '7 / 2 = ' + str( 7 / 2 ) + '<br>'
-        output = output + '1 / 3 = ' + str( 1 / 3 ) + '<br>'
-        output = output + '1 / 1 = ' + str( 1 / 1 ) + '<br>'
-        output = output + '<br>'
-        output = output + '整数での除算<br>'
-        output = output + '7 // 1 = ' + str( 7 // 1 ) + '<br>'
-        output = output + '7 // 2 = ' + str( 7 // 2 ) + '<br>'
-        output = output + '1 // 3 = ' + str( 1 // 3 ) + '<br>'
-        output = output + '余剰<br>'
-        output = output + '7 % 1 = ' + str( 7 % 1 ) + '<br>'
-        output = output + '7 % 2 = ' + str( 7 % 2 ) + '<br>'
-        output = output + '1 % 3 = ' + str( 1 % 3 ) + '<br>'
-        output = output + '1 % 1 = ' + str( 1 % 1 ) + '<br>'
-        output = output + '999 % 1000 = ' + str( 999 % 1000 ) + '<br>'
         
+        import math
+
+        output = ''
+        output = output + '平方根<br>'
+        output = output + 'math.sqrt(2) = ' + str( math.sqrt(2) ) + '<br>'
         output = output + '<br>'
-
-        output = output + 'ゼロ除算<br>'
-        try:
-            output = output + '1 / 0 = ' + str( 1 / 0 ) + '<br>'
-        except ZeroDivisionError as e:
-            output = output + '1 / 0 = ' + str(e) + '<br>'
-
-        try:
-            output = output + '1 // 0 = ' + str( 1 // 0 ) + '<br>'
-        except ZeroDivisionError as e:
-            output = output + '1 // 0 = ' + str(e) + '<br>'
-
-        try:
-            output = output + '1 % 0 = ' + str( 1 % 0 ) + '<br>'
-        except ZeroDivisionError as e:
-            output = output + '1 % 0 = ' + str(e) + '<br>'
+        output = output + '円周率<br>'
+        output = output + 'math.pi = ' + str( math.pi ) + '<br>'
+        output = output + '<br>'
+        output = output + '円周率<br>'
+        output = output + 'math.pi = ' + str( math.pi ) + '<br>'
+        output = output + '<br>'
+        output = output + '練習問題<br>'
+        output = output + '黄金比を求めてください。黄金比とは、5 の平方根に 1 を加えて 2 で割ったものです。約 1.618 になるはずです。<br>'
+        output = output + '( math.sqrt(5) + 1 ) / 2 = ' + str( ( math.sqrt(5) + 1 ) / 2 ) + '<br>'
             
 
         return output
